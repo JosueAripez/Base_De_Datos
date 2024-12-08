9695a2f7f02eb968fec3e5de6310c2536cc66a4d
JosueAripez <josue.aripez@uabc.edu.mx>
2024-12-08 11:54:27 -0800

    JS

---
+++ Proyecto/src/inicio.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Jockey+One&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Junge&display=swap" rel="stylesheet" />
+    <link href="./css/main.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="v1_185">
+        <div class="v1_186"></div>
+        <div class="v1_187"></div>
+        <div class="v1_188"></div>
+        <div class="v1_189"></div>
+        <div class="v1_190"></div>
+        <div class="v1_192"></div>
+        <div class="v1_200"></div>
+        <div class="v1_201"></div>
+        <div class="v1_202"></div>
+        <div class="v1_203"></div>
+        <div class="v1_204"></div>
+        <div class="v1_205"></div>
+        <div class="v1_206"></div><span class="v1_207">Bienvenidos: Hablemos de la Prevención del Embarazo
+            La prevención del embarazo es un tema esencial para cuidar la salud, el bienestar y las metas personales de
+            los adolescentes. Conocer sobre el cuerpo, las opciones anticonceptivas y la importancia de tomar decisiones
+            responsables es el primer paso para construir un futuro pleno y sin sorpresas inesperadas. Aquí encontrarás
+            información clara sobre los métodos disponibles, desde los preservativos hasta métodos hormonales, así como
+            la importancia de combinar protección para prevenir enfermedades de transmisión sexual (ETS).
+
+
+            Además, la prevención del embarazo no solo se trata de métodos, sino también de comunicación y educación.
+            Hablar abiertamente con amigos, familiares o profesionales de la salud sobre sexualidad y relaciones puede
+            marcar una gran diferencia. Este espacio está diseñado para ofrecerte las herramientas necesarias para que
+            tomes decisiones informadas y responsables, respetando siempre tus valores y objetivos personales.</span>
+        <div class="name"></div>
+        <div class="name"></div><span class="v1_210">Bienvenidos</span>
+        <div class="v1_211"></div><span class="v1_212">Por otro lado, el banco de sangre es un recurso vital que
+            garantiza la disponibilidad de transfusiones seguras, salvando vidas en situaciones críticas. Juntos, estos
+            temas resaltan la importancia de la educación y los recursos médicos en la construcción de una sociedad más
+            saludable.</span>
+        <div class="name"></div>
+        <div class="name"></div>
+        <div class="v1_191"></div>
+        <div class="v15_30"></div><span class="v15_32">La prevención del embarazo no deseado es una decisión informada
+            que te permite cuidar tu salud, tus metas y tu bienestar. Entender cómo funciona tu cuerpo, conocer los
+            métodos anticonceptivos y aprender a identificar riesgos son pasos esenciales para tomar el control de tu
+            vida sexual y reproductiva. Aquí encontrarás información confiable y herramientas prácticas para que puedas
+            decidir de manera consciente y responsable.
+
+
+            Además, la prevención es mucho más que usar anticonceptivos. Implica educación, comunicación y respeto hacia
+            uno mismo y los demás. Hablar sobre sexualidad y relaciones de forma abierta, ya sea con profesionales de la
+            salud o con personas de confianza, te ayudará a aclarar dudas y fortalecer tu capacidad de tomar decisiones
+            seguras que se alineen con tus planes y valores.</span><span class="v15_33">Conoce y Decide: La clave para
+            la prevención del Embarazo</span><span class="v87_145">Hablemos de la prevención del embarazo</span>
+        <div class="v82_198">
+            <div class="v82_199">
+                <div class="name"></div>
+                <div class="name"></div>
+                <div class="v82_202">
+                    <div class="v82_203"></div>
+                </div>
+            </div>
+            <div class="name"></div>
+            <div class="v82_205">
+                <div class="name"></div>
+                <div class="name"></div>
+            </div>
+        </div>
+        <div class="v87_136"></div><span class="v87_137">Arenas, 151, Fracc. Playa Ensenada, Ensenada BC,
+            México</span><span class="v87_138">(646) 173 - 4500</span><span
+            class="v87_139">contacto@hospitalvelmar.com</span>
+        <div class="name"></div>
+        <div class="v1_214"></div>
+        <div class="v92_194"></div>
+    </div>
+</body>
+
+</html>
