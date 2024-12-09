57af1deea2605919162f124ec0244eaa3aa26289
JosueAripez <josue.aripez@uabc.edu.mx>
2024-12-08 16:57:04 -0800

    Cambios

--- Proyecto/src/recuperacion.php
+++ Proyecto/src/recuperacion.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Plataforma de Prevención del Embarazo</title>
+    <title>Plataforma de Prevención</title>
     <link href="https://fonts.googleapis.com/css?family=Krona+One&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Kufam&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-q2P7zyhCbHp0He6Nj3A2hZ9JFsqJrNAXW3pIJ8zhm3Z2jmxukv9Umf4Vp0RY1z1J" crossorigin="anonymous">
